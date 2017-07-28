2b1fe083d9ea1f4459f560ebc1ff741316f95b44
wiancrew <30219452+wiancrew@users.noreply.github.com>
2017-07-28 22:05:41 +0000

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,40 @@
+
+def checkInput(selection):
+  if selection not in ["rock", "paper", "scissors"]:
+    print "Player Made Invalid Selection.Exiting"
+    exit()
+
+keepPlaying = "yes"
+while keepPlaying == "yes":
+
+  player1 = raw_input("Player 1 Picks: ")
+  player2 = raw_input("Player 2 Picks: ")
+
+  checkInput(player1)
+  checkInput(player2)
+
+  print player1
+  print player2
+  
+  if player1 == player2:
+    print "It's a tie!"
+ 
+  if player1 == "rock":
+    if player2 == "paper":
+       print "Player 2 wins"
+  if player2  == "scissors":
+      print "Player 1 wins"
+ 
+  if  player1 == "paper":
+    if player2 == "scissors":
+      print "Player 2 wins"
+    if player2 == "rock":
+      print "Player 1 wins"
+    
+  if player1 == "scissors":
+    if player2 == "papper":
+      print "Player 1 wins"
+    if player2 == "rock":
+      print "Player 2 wins"
+  keepPlaying =raw_input("Do you want to keep playing? (yes/no)")
+  print "Game Over!"
