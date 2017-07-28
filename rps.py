7f330be445fa5e4ee96c11c903778e1d421ed6fe
wiancrew <30219452+wiancrew@users.noreply.github.com>
2017-07-28 22:10:52 +0000

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,4 +1,22 @@
+    rps - The Classic Game of Rock Paper Scissors! 
+    Copyright (C) 2017 - Rahul Chaudhary 
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contact us at: 2520@tuta.io
+
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 def checkInput(selection):
   if selection not in ["rock", "paper", "scissors"]:
     print "Player Made Invalid Selection.Exiting"
